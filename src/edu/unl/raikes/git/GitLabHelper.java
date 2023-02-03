b11d0cd038a5ce974aecad6c12aa1d837a641d46
GowravG <gghatamaneni2@huskers.unl.edu>
2023-02-03 12:59:04 -0600

    Changed HitLab and helper class x2

--- src/edu/unl/raikes/git/GitLabHelper.java
+++ src/edu/unl/raikes/git/GitLabHelper.java
@@ -1,5 +1,12 @@
 package edu.unl.raikes.git;
-
+import java.util.Arrays;
 public class GitLabHelper {
-
+	private String[] persons;
+	GitLabHelper(String[] persons) {
+		this.persons = persons;
+	}
+	public void PrintWelcomeMessage() {
+		System.out.print("This is a lesson on git made by: " +
+				Arrays.toString(persons));
+	}
 }
